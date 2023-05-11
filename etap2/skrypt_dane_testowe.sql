a3f2e1b00ad307c888f2c7c7e19f6b553cbffcf3
Richard Staszkiewicz <richard.staszkiewicz@gmail.com>
2023-05-11 12:25:37 +0200

    [Reports] Added 1 new FAKTURA entry

--- etap2/skrypt_dane_testowe.sql
+++ etap2/skrypt_dane_testowe.sql
@@ -58,6 +58,17 @@ INSERT INTO POZYCJE_FAKTUR VALUES
 INSERT INTO POZYCJE_FAKTUR VALUES
 (2, 2, 'Dostarczenie kateringu', 1, 2, sysdate-60, 7, 2, 9);
 
+INSERT INTO FAKTURY 
+(id_faktury, nr_faktury, id_nabywcy, id_sprzedawcy, data_wystawienia, data_zaplaty, termin_zaplaty, stawka_vat, sposob_zaplaty, netto, vat, brutto, wartosc_kwoty_slownie)
+VALUES
+(3, '03/2023', 3, 1, sysdate-10, sysdate-11, sysdate-15, 19, '0', 251.5, 59, 310.5, 'trzysta dziesiêæ zloty i piêædziesiat groszy');
+
+INSERT INTO POZYCJE_FAKTUR VALUES
+(3, 1, 'Boczek w ciescie', 7, 2, sysdate-10, 227.5, 53, 279.5);
+
+INSERT INTO POZYCJE_FAKTUR VALUES
+(3, 2, 'Dostarczenie kateringu', 1, 2, sysdate-10, 25, 6, 31);
+
 -- ZAMOWIENIA
 INSERT INTO ZAMOWIENIA VALUES (1, sysdate-60, 2, 3);
 INSERT INTO ZAMOWIENIA VALUES (2, sysdate-40, 2, 4);
